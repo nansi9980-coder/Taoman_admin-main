3f6ac79909fe346f278f36391b902106a73787ba
Mambizah <ferytail360@gmail.com>
2026-05-20 02:50:13 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ export default function App() {
     <ThemeProvider>
       <AuthProvider>
         <AppProvider>
-          <BrowserRouter>
+          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <AppRoutes />
           </BrowserRouter>
         </AppProvider>
